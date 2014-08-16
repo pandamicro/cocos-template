4962fdac82a93330d485d793ee208a15dbb85314
pandamicro <lphuabin@gmail.com>
2014-08-16 16:20:13 +0800

    Add cool loading

--- xyt/frameworks/cocos2d-html5/cocos2d/core/scenes/CCLoaderScene.js
+++ xyt/frameworks/cocos2d-html5/cocos2d/core/scenes/CCLoaderScene.js
@@ -25,51 +25,58 @@
 
 cc.LoaderScene = cc.Scene.extend({
     _interval : null,
+    _length : 0,
+    _count : 0,
     _label : null,
     _className:"LoaderScene",
+    _drawLoading:null,
 
+    ctor : function() {
+        this._super();
+
+        this._drawLoading = (function() {
+            var canvas = cc._canvas, ctx = canvas.getContext("2d"), w = canvas.width, h = canvas.height, x = w / 2, y = h / 2, r = 8, d = 22, pos = [[44,22], [44,0], [22,0], [0,0], [0,22], [0,44], [22,44]], PI2 = Math.PI* 2, rx, ry;
+            var dt_move = 11, dt_wait = 3, dt = dt_move + dt_wait, t = 0, total_t = dt * 7, ds = 22 / dt_move, curr_t = 0, direction = 0, mo = 2.5;
+            return function () {
+                if(t == total_t + 15) t = 0;
+                curr = Math.floor(t / dt), curr_t = t % dt;
+                ctx.save();
+                ctx.setTransform(1, 0, 0, 1, x, y);
+                ctx.fillStyle = "#000"; ctx.fillRect(-x, -y, w, h);
+                ctx.strokeStyle = "#fff"; ctx.lineWidth = 2;
+                ctx.beginPath(); ctx.arc(0, 0, 60, 0, Math.PI*2); ctx.stroke();
+                ctx.fillStyle = "#fff"; ctx.translate(-d, -d);
+                for(var i = 0; i < 7; i++) {
+                    if(curr_t == dt_move && i == curr) {direction == 0 && (pos[i][1] += mo); direction == 1 && (pos[i][0] -= mo); direction == 2 && (pos[i][1] -= mo); direction == 3 && (pos[i][0] += mo);}
+                    if(curr_t == dt_move + 2 && i == curr) {direction == 0 && (pos[i][1] -= mo); direction == 1 && (pos[i][0] += mo); direction == 2 && (pos[i][1] += mo); direction == 3 && (pos[i][0] -= mo);}
+                    rx = pos[i][0], ry = pos[i][1];
+                    if(curr_t < dt_move && i == curr) {
+                        if(rx == 44 && ry < 44) (pos[i][1] = ry += ds) && (direction = 0);
+                        else if(ry == 44 && rx > 0) (pos[i][0] = rx -= ds) && (direction = 1);
+                        else if(rx == 0 && ry > 0) (pos[i][1] = ry -= ds) && (direction = 2);
+                        else if(ry == 0 && rx < 44) (pos[i][0] = rx += ds) && (direction = 3);
+                    }
+                    ctx.beginPath(); ctx.arc(rx, ry, r, 0, PI2); ctx.fill();
+                }
+                ctx.beginPath(); ctx.arc(22, 22, r, 0, PI2); ctx.fill();
+                ctx.restore();
+                t++;
+            };
+        })();
+    },
     init : function(){
-        var self = this;
+        this.scheduleUpdate();
 
-        //logo
-        var logoWidth = 160;
-        var logoHeight = 200;
-
-        // bg
-        var bgLayer = self._bgLayer = new cc.LayerColor(cc.color(32, 32, 32, 255));
-        bgLayer.setPosition(cc.visibleRect.bottomLeft);
-        self.addChild(bgLayer, 0);
-
-        //image move to CCSceneFile.js
-        var fontSize = 24, lblHeight =  -logoHeight / 2 + 100;
-        if(cc._loaderImage){
-            //loading logo
-            cc.loader.loadImg(cc._loaderImage, {isCrossOrigin : false }, function(err, img){
-                logoWidth = img.width;
-                logoHeight = img.height;
-                self._initStage(img, cc.visibleRect.center);
-            });
-            fontSize = 14;
-            lblHeight = -logoHeight / 2 - 10;
-        }
         //loading percent
-        var label = self._label = cc.LabelTTF.create("Loading... 0%", "Arial", fontSize);
-        label.setPosition(cc.pAdd(cc.visibleRect.center, cc.p(0, lblHeight)));
+        var label = this._label = cc.LabelTTF.create("加载中... 0%", "Arial", 24);
+        label.setPosition(cc.pAdd(cc.visibleRect.center, cc.p(0, -50)));
         label.setColor(cc.color(180, 180, 180));
-        bgLayer.addChild(this._label, 10);
+        this.addChild(this._label, 10);
         return true;
     },
 
-    _initStage: function (img, centerPos) {
-        var self = this;
-        var texture2d = self._texture2d = new cc.Texture2D();
-        texture2d.initWithElement(img);
-        texture2d.handleLoadedTexture();
-        var logo = self._logo = cc.Sprite.create(texture2d);
-        logo.setScale(cc.contentScaleFactor());
-        logo.x = centerPos.x;
-        logo.y = centerPos.y;
-        self._bgLayer.addChild(logo, 10);
+    update: function() {
+        this._drawLoading();
     },
 
     onEnter: function () {
@@ -80,7 +87,7 @@ cc.LoaderScene = cc.Scene.extend({
 
     onExit: function () {
         cc.Node.prototype.onExit.call(this);
-        var tmpStr = "Loading... 0%";
+        var tmpStr = "加载中... 0%";
         this._label.setString(tmpStr);
     },
 
@@ -99,15 +106,23 @@ cc.LoaderScene = cc.Scene.extend({
         var self = this;
         self.unschedule(self._startLoading);
         var res = self.resources;
-        cc.loader.load(res,
-            function (result, count, loadedCount) {
-                var percent = (loadedCount / count * 100) | 0;
-                percent = Math.min(percent, 100);
-                self._label.setString("Loading... " + percent + "%");
-            }, function () {
-                if (self.cb)
-                    self.cb();
-            });
+        self._length = res.length;
+        self._count = 0;
+        cc.loader.load(res, function(result, count){ self._count = count; }, function(){
+            if(self.cb)
+                self.cb();
+        });
+        self.schedule(self._updatePercent);
+    },
+
+    _updatePercent: function () {
+        var self = this;
+        var count = self._count;
+        var length = self._length;
+        var percent = (count / length * 100) | 0;
+        percent = Math.min(percent, 100);
+        self._label.setString("加载中... " + percent + "%");
+        if(count >= length) self.unschedule(self._updatePercent);
     }
 });
 cc.LoaderScene.preload = function(resources, cb){
