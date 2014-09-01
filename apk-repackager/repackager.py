1c9b57cf842184f16fb66cef3dab5a3a980a8037
pandamicro <lphuabin@gmail.com>
2014-09-01 14:54:08 +0800

    Ignore single file engine in repackager

--- apk-repackager/repackager.py
+++ apk-repackager/repackager.py
@@ -9,7 +9,7 @@ import errno
 import commands
 
 keep_in_assets = ["assets/script"]
-skip_in_src = ["CMakeLists.txt", "frameworks", "index.html", "runtime", "tools", ".cocos-project.json", ".DS_Store", ".idea"]
+skip_in_src = ["cocos2d-html5.js", "CMakeLists.txt", "frameworks", "index.html", "runtime", "tools", ".cocos-project.json", ".DS_Store", ".idea"]
 storepass = "android"
 keypass = "android"
 keystore = "androiddebugkey"
